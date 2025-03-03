3fdc8283fe535a28eb927e7b9ab38023dc89c65e
thirstu <ajayknpc185@gmail.com>
2025-03-03 22:05:21 +0530

    userDetails userAvatar update controllers

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -4,6 +4,8 @@ import {User} from "../models/user.models.js"
 // import { upload } from "../middleware/multer.middleware.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import jwt from 'jsonwebtoken'
+import { env_file } from "../index_env.js";
 
 
 const generateAccessAndRefreshTokens =async (userId)=>{
@@ -130,7 +132,7 @@ const loginUser=asyncHandler(async(req,res,next)=>{
     const {email, password,userName}=req.body;
     console.log("-130--loginUser",req.body,req.files,req.params,req.query);
     // console.log("-131--loginUser",req);
-    if(!email || !password){
+    if(!(email || password)){
         throw new ApiError(400,"userName or email ir required");
     }
 
@@ -214,8 +216,144 @@ const logoutUser=asyncHandler(async (req, res) => {
 })
 
 
+
+
+const refreshAccessToken =asyncHandler(async (req, res) => {
+  try {
+      const incomingRefreshToken =await req.cookies.refreshToken||req.body.refreshToken;
+  
+      if(!incomingRefreshToken){
+          throw new ApiError(401,"unauthorized request");
+      }
+  
+      const decodedToken=jwt.verify(
+          incomingRefreshToken,
+          env_file.REFRESH_TOKEN_SECRET
+      );
+  
+      const user=await User.findById(decodedToken?._id);
+      
+  
+      if(!user){
+          throw new ApiError(401,"invalid refresh token");
+      }
+  
+      if(incomingRefreshToken !== user?.refreshToken){
+          throw new ApiError(401,"refresh token is expired or used"); 
+      }
+  
+  
+      const options={
+          httpOnly: true,
+          secure: true,
+      }
+  
+      const {accessToken,newRefreshToken}=await generateAccessAndRefreshTokens(user._id);
+  
+      return res
+      .status(200)
+      .cookie("accessToken",accessToken)
+      .cookie("accessToken",newRefreshToken)
+      .json(
+          new ApiResponse(200,{accessToken,refreshToken:newRefreshToken},"access token refreshed ")
+      )
+  
+  } catch (err) {
+    console.error("-262--refreshAccessToken",err);
+    throw new ApiError(401,err?.message || "Invalid refresh token")
+    
+  }
+
+})
+
+const changeCurrentPassword =asyncHandler(async (req,res)=>{
+    const {oldPassword,newPassword} = req.body;
+
+    const user=await User.findById(req.user?._id);
+    const isPasswordCorrect=await user.isPasswordCorrect(oldPassword);
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400,"Invalid old password")
+    }
+
+    user.password=newPassword;
+    await user.save({validateBeforeSave:false});
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{},"Password updated successfully"))
+})
+
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(200,req.user,"current user fetched successfully")
+})
+
+const updateAccountDetails= asyncHandler(async(req,res)=>{
+    const {fullName, email}=req.body;
+
+    if(!fullName||!email){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const user=await User.findByIdAndUpdate(req.user?._id,{
+        $set:{
+            fullName:fullName,
+            email:email
+        }
+
+    },
+    {new:true}
+).select("-password");
+
+return res
+.status(200)
+.json(new ApiResponse(200,user,"account updated successfully"))
+
+})
+
+const updateUserAvatar=asyncHandler(async (req,res)=>{
+    const avatarLocalPath=req.file?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is missing")
+    }
+
+    const avatar =await uploadOnCloudinary(avatarLocalPath);
+
+    if(!avatar?.url){
+        throw new ApiError(400,"error while uploading avatar")
+
+    }
+
+    const user=await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user,"user avatar updated successfully")
+    )
+
+})
+
+
+
 export { 
     registerUser,
     loginUser,
     logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar
 }
---
+++ server/src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose,{Schema} from "mongoose";
+
+
+const subscriptionSchema=new Schema({
+
+    subscriber:{
+        type:Schema.Types.ObjectId,////the one who subscribes
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,////the one who get subscribed
+        ref:"User"
+    }
+
+},{timestamps:true})
+
+
+export const Subscription=mongoose.model("Subscription",subscriptionSchema);
